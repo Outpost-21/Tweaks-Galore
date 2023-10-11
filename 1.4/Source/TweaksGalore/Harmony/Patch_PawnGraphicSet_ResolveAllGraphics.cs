2a700278454e4619c04eb6a0da2c52ab779d6d4c
neronix17 <39186138+neronix17@users.noreply.github.com>
2023-10-11 07:57:28 +0100

    3.0.8684

---
+++ 1.4/Source/TweaksGalore/Harmony/Patch_PawnGraphicSet_ResolveAllGraphics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+using HarmonyLib;
+
+namespace TweaksGalore
+{
+    [HarmonyPatch(typeof(PawnGraphicSet), "ResolveAllGraphics")]
+    public class Patch_PawnGraphicSet_ResolveAllGraphics
+	{
+        [HarmonyPostfix]
+        public static void Postfix(PawnGraphicSet __instance)
+		{
+            if (TGTweakDefOf.Tweak_DisableSpecialSkinShader.BoolValue)
+			{
+				if (__instance.pawn.RaceProps.Humanlike)
+				{
+					LogUtil.LogMessage("Cutout Skin Shader Used");
+					__instance.nakedGraphic = GraphicDatabase.Get<Graphic_Multi>(__instance.pawn.story.bodyType.bodyNakedGraphicPath, ShaderDatabase.Cutout, Vector2.one, __instance.pawn.story.SkinColor);
+					PortraitsCache.SetDirty(__instance.pawn);
+					GlobalTextureAtlasManager.TryMarkPawnFrameSetDirty(__instance.pawn);
+				}
+			}
+		}
+	}
+}
--- 1.4/Source/TweaksGalore/Harmony/Patch_ShaderUtility_GetSkinShader.cs
+++ 1.4/Source/TweaksGalore/Harmony/Patch_ShaderUtility_GetSkinShader.cs
@@ -15,17 +15,16 @@ namespace TweaksGalore
     [HarmonyPatch(typeof(ShaderUtility), "GetSkinShader")]
     public class Patch_ShaderUtility_GetSkinShader
 	{
-        [HarmonyPrefix]
-        public static bool Prefix(ref Shader __result, bool skinColorOverriden)
+        [HarmonyPostfix]
+        public static void Postfix(ref Shader __result, bool skinColorOverriden)
 		{
             if (TGTweakDefOf.Tweak_DisableSpecialSkinShader.BoolValue)
 			{
 				LogUtil.LogMessage("Cutout Skin Shader Used");
 				__result = ShaderDatabase.Cutout;
-				return false;
+				return;
 			}
 			__result = (skinColorOverriden ? ShaderDatabase.CutoutSkinColorOverride : ShaderDatabase.CutoutSkin);
-			return true;
 		}
 	}
 }
